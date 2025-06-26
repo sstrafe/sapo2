5b86a588ea448bfd1ba19564600b8c6c472c432e
i202408959 <i202408959@cibertec.edu.pe>
2025-06-26 18:33:26 -0500

    2do commit

---
+++ src/main/java/com/cibertec/Sapo1Application.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sapo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sapo1Application.class, args);
+	}
+
+}
